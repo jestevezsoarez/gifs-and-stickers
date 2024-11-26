09275435defbe3645c0aab1b887d3f05910d040f
Javier Estevez <javierdaniloestevez@gmail.com>
2024-11-26 01:52:31 -0300

    using viewchild to the the input value of the search

--- src/app/gifs/components/search/search.component.ts
+++ src/app/gifs/components/search/search.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
+  @ViewChild('txtTagInput') tagInput!: ElementRef<HTMLInputElement>;
 
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
 }
