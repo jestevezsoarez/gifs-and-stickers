3710f328f3d9fe060b01fa4a78705355c2d0d826
Javier Estevez <javierdaniloestevez@gmail.com>
2024-11-27 18:00:40 -0300

    organize history of gifs and limit search to 10

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,21 +1,32 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GifsService {
-
   private _tagsHistory: string[] = [];
 
-  constructor() { }
+  constructor() {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
+  private organizeHistory(tag: string) { 
+    if (this._tagsHistory.includes(tag)) { 
+      this._tagsHistory = this._tagsHistory.filter((elem) => elem !== tag);
+    }
+
+    this._tagsHistory = this._tagsHistory.splice(0, 9);
+  }
+
   public searchTag(tag: string): void {
+    if (tag.length === 0) return;
+
+    tag = tag.toLowerCase();
+    this.organizeHistory(tag);
+
     this._tagsHistory.unshift(tag);
-        
   }
 
 }
